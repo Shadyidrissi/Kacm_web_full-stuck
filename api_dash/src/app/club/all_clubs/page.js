2f3553e0a4d7357f04ebeb4717ccc17512266663
Shady Idrissi <chadiidac@gmail.com>
2025-02-09 13:54:09 +0100

    add sweet aleart on page 'all clubs'

--- api_dash/src/app/club/all_clubs/page.js
+++ api_dash/src/app/club/all_clubs/page.js
@@ -1,21 +1,59 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
+"use client";
+import Navbar from "@/components/Navbar";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Swal from "sweetalert2";
 
-function page() {
+function Page() {
+    const router = useRouter(); 
+
+    const handelClick = (id) => {  // Accept ID as a parameter
+        Swal.fire({
+            title: "Edit This Club",
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: "Edit",
+            denyButtonText: "Delete"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                router.push(`/clubs/edit/${id}`); // Pass ID in the URL
+            } else if (result.isDenied) {
+                Swal.fire({
+                    title: "Are you sure?",
+                    text: "Do you want to delete this?",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Delete",
+                    cancelButtonText: "Cancel",
+                    dangerMode: true
+                }).then((deleteResult) => {
+                    if (deleteResult.isConfirmed) {
+                        console.log(`Deleted item with ID: ${id}`); // Perform delete action
+                        Swal.fire("Deleted!", "Your item has been deleted.", "success");
+                    }
+                });
+            }
+        });
+    };
+
     return (
         <div className="home-page">
             <Navbar />
-            <div className='club'>
-                <h2 className='title'>Title</h2>
-                <div className='cards-clubs'>
-                    <a href='' className='card-club'>
-                        <img src="https://upload.wikimedia.org/wikipedia/ar/9/9e/%D8%B4%D8%B9%D8%A7%D8%B1_%D9%86%D8%A7%D8%AF%D9%8A_%D8%A7%D9%84%D9%83%D9%88%D9%83%D8%A8_%D8%A7%D9%84%D9%85%D8%B1%D8%A7%D9%83%D8%B4%D9%8A.png" alt="" />
-                        <h3>name</h3>
-                    </a>
+            <div className="club">
+                <h2 className="title">Title</h2>
+                <div className="cards-clubs">
+                    {/* Pass ID (example: 123) when clicking the button */}
+                    <button onClick={() => handelClick(123)} className="card-club">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/ar/9/9e/%D8%B4%D8%B9%D8%A7%D8%B1_%D9%86%D8%A7%D8%AF%D9%8A_%D8%A7%D9%84%D9%83%D9%88%D9%83%D8%A8_%D8%A7%D9%84%D9%85%D8%B1%D8%A7%D9%83%D8%B4%D9%8A.png"
+                            alt=""
+                        />
+                        <h3>Club Name</h3>
+                    </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default page
+export default Page;
